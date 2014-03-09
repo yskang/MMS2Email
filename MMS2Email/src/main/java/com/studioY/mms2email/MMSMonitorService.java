b7eff8a3746ae8bc223bd485839bd982ba4e1de7
yongsung.kang <yongsung.kang@gmail.com>
2014-03-09 12:58:57 +0900

    change notification api and add activity activation function

--- MMS2Email/src/main/java/com/studioY/mms2email/MMSMonitorService.java
+++ MMS2Email/src/main/java/com/studioY/mms2email/MMSMonitorService.java
@@ -1,13 +1,17 @@
 package com.studioY.mms2email;
 
 import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
+
 
 public class MMSMonitorService extends Service {
     private MMSMonitor mmsMonitor;
@@ -33,8 +37,25 @@ public class MMSMonitorService extends Service {
         intentFilter.addAction(Commons.COM_STUDIO_Y_MMS2EMAIL_CHECK_MMSMONITOR);
         registerReceiver(broadcastReceiver, intentFilter);
 
-        Notification notification = new Notification(R.drawable.ic_launcher, "MMS 전달 서비스", System.currentTimeMillis());
-        notification.setLatestEventInfo(getApplicationContext(), "MMS 전달 서비스", "MMS가 수신되면 지정된 e-mail로 전송됩니다.", null);
+//        Notification notification = new Notification(R.drawable.ic_launcher, "MMS 전달 서비스", System.currentTimeMillis());
+//        notification.setLatestEventInfo(getApplicationContext(), "MMS 전달 서비스", "MMS가 수신되면 지정된 e-mail로 전송됩니다.", null);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle("MMS 전달 서비스 실행중")
+                .setContentText("MMS가 수신 되면 지정된 e-mail 주소로 전송됩니다.");
+
+        Intent resultIntent = new Intent(this, MMS2EmailMainActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MMS2EmailMainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(resultPendingIntent);
+
+        Notification notification = builder.build();
+
         startForeground(1, notification);
         startForeground(startId, notification);
 
